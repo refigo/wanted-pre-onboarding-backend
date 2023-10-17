b73dc0c03653a5ef9062ba19ae86f219b800eb9b
Refi Go <64242999+refigo@users.noreply.github.com>
2023-10-17 19:55:35 +0900

    fix: Set unique columns in JobApplicationEntity - #5

--- src/database/entities/job.application.entity.ts
+++ src/database/entities/job.application.entity.ts
@@ -1,9 +1,10 @@
-import { Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn, Unique } from "typeorm";
 import { CompanyEntity } from "./company.entity";
 import { UserEntity } from "./user.entity";
 import { JobRecruitmentEntity } from "./job.recruitment.entity";
 
 @Entity({ name: 'JOB_APPLICATION' })
+@Unique(['userEntity', 'jobRecruitmentEntity'])
 export class JobApplicationEntity {
 	// id
 	@PrimaryGeneratedColumn({ name: 'id', type: 'bigint' })
