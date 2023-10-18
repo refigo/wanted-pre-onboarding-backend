fc58df2ef165db41c9cda56328a97f93eb866ffb
Refi Go <64242999+refigo@users.noreply.github.com>
2023-10-19 00:18:36 +0900

    feat: Set ResponseDetailsJobRecruitmentDto - #9

---
+++ src/job/recruitments/dto/response-details-job-recruitment.dto.ts
@@ -0,0 +1,11 @@
+export class ResponseDetailsJobRecruitmentDto {
+	recruitment_id: number;
+	company_name: string;
+	nation: string;
+	area: string;
+	position: string;
+	compensation: number;
+	skills: string;
+	contents: string;
+	recruitment_ids_of_company: number[];
+}
