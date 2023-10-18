b01c8533394c276572bf5f9286ee433d48ac651c
Refi Go <64242999+refigo@users.noreply.github.com>
2023-10-18 22:18:02 +0900

    feat: Add ResponseJobRecruitmentDto - #8

---
+++ src/job/recruitments/dto/response-job-recruitment.dto.ts
@@ -0,0 +1,9 @@
+export class ResponseJobRecruitmentDto {
+	recruitment_id: number;
+	company_name: string;
+	nation: string;
+	area: string;
+	position: string;
+	compensation: number;
+	skills: string;
+}
