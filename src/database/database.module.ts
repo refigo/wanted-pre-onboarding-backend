ba0349bc0bbe4fa0a0da9ee8021435ff02111fa5
Refi Go <64242999+refigo@users.noreply.github.com>
2023-10-19 00:29:06 +0900

    fix: Remove TypeOrmModule.forFeature in DatabaseModule

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,19 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CompanyEntity } from './entities/company.entity';
-import { UserEntity } from './entities/user.entity';
 import { typeORMConfig } from 'src/configs/typeorm.config';
-import { JobRecruitmentEntity } from './entities/job.recruitment.entity';
-import { JobApplicationEntity } from './entities/job.application.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeORMConfig),
-    TypeOrmModule.forFeature([
-      CompanyEntity,
-      UserEntity,
-      JobRecruitmentEntity,
-      JobApplicationEntity
-  ])],
+    TypeOrmModule.forRoot(typeORMConfig)
+  ],
 })
 export class DatabaseModule {}
