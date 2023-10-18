b86644b00c20f24d712faa2dcbc741ecc02a6a8f
Refi Go <64242999+refigo@users.noreply.github.com>
2023-10-19 08:51:40 +0900

    feat: Set configuration to use env for database - #12

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeORMConfig } from 'src/configs/typeorm.config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeORMConfig)
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST') || 'localhost',
+        port: configService.get('DB_PORT') || 5432,
+        username: configService.get('DB_USER') || 'postgres',
+        password: configService.get('DB_PASS') || 'postgres',
+        database: configService.get('DB_NAME') || 'test',
+        entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        synchronize: true
+      }),
+    }),
   ],
 })
 export class DatabaseModule {}
